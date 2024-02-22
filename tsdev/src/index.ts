10ca27a4c51e5b7213eb35eab412697e9d189b84
nick5595 <nick55955@gmail.com>
2024-02-22 10:51:18 +0100

    done

--- tsdev/src/index.ts
+++ tsdev/src/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import { Queue } from "./Queue/Queue";
 import bodyParser from "body-parser";
+let ejs = require('ejs');
 
 // class QueueModel {
 //     private queue: Queue<any> = new Queue<any>();
@@ -22,6 +23,7 @@ import bodyParser from "body-parser";
 // }
 class QueueModel {
     public queue: Queue<any> = new Queue<any>();
+
     enqueue(item: any): void {
         this.queue.enqueue(item);
     }
@@ -36,13 +38,23 @@ class QueueModel {
      length(): number {
          return this.queue.length();
      }
+     constructor(){
+        this.queue.setCapacity(4);
+     }
  
 }
 class QueueView {
-    render(queue: Queue<any>): string {
+    render(queue: Queue<any>, value: any): string {
         // const todoList = queue
         //     .map((item) => `<li>${index + 1}. ${item} <a href="/delete/${index}">Delete</a></li>`)
         //     .join('');
+        let bla: string = "";
+        if (value === undefined) {
+            bla = `<p>Die Queue ist leer</p>`
+        }
+        if (value) {
+            bla = `<p>Der erste Wert der Queue ist: ${value}</p>`
+        }
         return `
             <!DOCTYPE html>
             <html>
@@ -50,7 +62,8 @@ class QueueView {
                 <title>Queue</title>
             </head>
             <body>
-                <p>Die Queue hat ${queue.length()} Elemente.</p>
+                <p>Die Queue hat ${queue.length()} von ${queue.getCapacity()} Elemente.</p>
+                ${bla}
             </body>
             </html>
         `;
@@ -100,24 +113,37 @@ const controller = new QueueController(model, view);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//app.set('view engine', 'ejs');
 
 app.get("/", (req: Request, res: Response) => {
     const queue = controller.getQueue();
-    const html = view.render(queue);
+    const html = view.render(queue, null);
     res.send(html);
 });
 
 app.post("/enqueue", (req: Request, res: Response) => {
     const item = req.body.value;
     if (item) {
-        controller.enqueue(item);
+        try{
+            controller.enqueue(item);
+        }
+        catch(exception){
+            res.status(418).send(exception);
+        }
     }
     res.redirect('/');
 });
 app.get("/dequeue", (req: Request, res: Response) => {
         let value = controller.dequeue();
-
-    res.redirect('/');
+        const queue = controller.getQueue();
+        const html = view.render(queue, value);
+    res.send(html);
+});
+app.get("/peek", (req: Request, res: Response) => {
+        let value = controller.peek();
+        const queue = controller.getQueue();
+        const html = view.render(queue, value);
+    res.send(html);
 });
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
